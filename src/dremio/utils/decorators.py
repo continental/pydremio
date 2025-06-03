f93649c87d4a47e70c491dbd7c733e3eb4efcf06
Jan <jan-d.pietsch@live.de>
2025-06-03 09:39:30 +0200

    extend utils with experimental decorator

--- src/dremio/utils/decorators.py
+++ src/dremio/utils/decorators.py
@@ -1,13 +1,12 @@
-import functools
+import functools, warnings, logging
 from typing import Callable
-import logging
 
 
 def deprecated(hint: str):
     def decorator(func: Callable):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            logging.warn(
+            warnings.warn(
                 DeprecationWarning(f"{func.__name__} is deprecated. {hint}"),
                 stacklevel=2,
             )
@@ -28,5 +27,30 @@ def deprecated_interface(func: Callable):
         logging.warning(DeprecationWarning(s), stacklevel=2)
         func.__doc__ = s + "\n" + (func.__doc__ or "")
         return func(*args, **kwargs)
+
+    return wrapper 
+
+
+already_warned = False
+
+def experimental(func):
+    note = "\n\n⚠️ EXPERIMENTAL: This method may change or be removed in future versions.\n"
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        global already_warned
+        if not already_warned:
+            warnings.warn(
+                f"The function '{func.__name__}' is experimental and may change or be removed in future versions.",
+                category=UserWarning,
+                stacklevel=2
+            )
+            already_warned = True
+        return func(*args, **kwargs)
+
+    # Modify the docstring
+    doc = func.__doc__ or ""
+    if note.strip() not in doc:
+        wrapper.__doc__ = doc + note
 
     return wrapper
